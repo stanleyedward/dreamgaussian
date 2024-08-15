da1397d00df8001bfa70a5e7c048789d55d93c33
stanleyedward <114278820+stanleyedward@users.noreply.github.com>
2024-08-15 16:33:16 +0530

    change

--- engine.sh
+++ engine.sh
@@ -4,8 +4,8 @@ CONFIG="${2:-image}"
 ELEVATION=${3:-0}
 
 python process.py data/$TARGET.png --size 1000
-python main.py --config configs/$CONFIG.yaml input=data/$TARGET_rgba.png save_path=$TARGET mesh_format=glb elevation=ELEVATION force_cuda_rast=True
-python main2.py --config configs/$CONFIG.yaml input=data/$TARGET_rgba.png save_path=$TARGET mesh_format=glb elevation=ELEVATION force_cuda_rast=True
+python main.py --config configs/$CONFIG.yaml input=data/$TARGET_rgba.png save_path=$TARGET mesh_format=glb elevation=$ELEVATION force_cuda_rast=True
+python main2.py --config configs/$CONFIG.yaml input=data/$TARGET_rgba.png save_path=$TARGET mesh_format=glb elevation=$ELEVATION force_cuda_rast=True
 
 
 # python main.py --config configs/imagedream.yaml input=data/$TARGET_rgba.png prompt="mcdondalds burger" save_path=$TARGET mesh_format=glb elevation=0 force_cuda_rast=True
