efd1ba7774b3bb01ceab1955cfb0d824964d894f
stanleyedward <114278820+stanleyedward@users.noreply.github.com>
2024-08-15 13:55:08 +0530

    add conda setup

--- setup.sh
+++ setup.sh
@@ -1,4 +1,21 @@
+mkdir -p ~/miniconda3
+wget https://repo.anaconda.com/miniconda/Miniconda3-latest-Linux-x86_64.sh -O ~/miniconda3/miniconda.sh
+bash ~/miniconda3/miniconda.sh -b -u -p ~/miniconda3
+rm -rf ~/miniconda3/miniconda.sh
+
+~/miniconda3/bin/conda init bash
+~/miniconda3/bin/conda init zsh
+
+source ~/.bashrc
+source ~/.zshrc 
+
+conda create -y -n meshdream python=3.10
+conda activate dg
+
+conda install pytorch==1.12.1 torchvision==0.13.1 torchaudio==0.12.1 cudatoolkit=11.6 -c pytorch -c conda-forge
+pip install xformers==0.0.12
 pip install -r requirements.txt
+# conda install -y libffi==3.3            
 
 # a modified gaussian splatting (+ depth, alpha rendering)
 git clone --recursive https://github.com/ashawkey/diff-gaussian-rasterization
