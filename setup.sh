a7c6c085a7b2e04a5a510596a83777cf2cc5b268
stanleyedward <114278820+stanleyedward@users.noreply.github.com>
2024-08-15 11:27:25 +0530

    add setup script

---
+++ setup.sh
@@ -0,0 +1,22 @@
+pip install -r requirements.txt
+
+# a modified gaussian splatting (+ depth, alpha rendering)
+git clone --recursive https://github.com/ashawkey/diff-gaussian-rasterization
+pip install ./diff-gaussian-rasterization
+
+# simple-knn
+pip install ./simple-knn
+
+# nvdiffrast
+pip install git+https://github.com/NVlabs/nvdiffrast/
+
+# kiuikit
+pip install git+https://github.com/ashawkey/kiuikit
+
+# To use MVdream, also install:
+pip install git+https://github.com/bytedance/MVDream
+
+# To use ImageDream, also install:
+pip install git+https://github.com/bytedance/ImageDream/#subdirectory=extern/ImageDream
+
+sudo apt-get install -y mesa-common-dev libglu1-mesa-dev libgmp-dev libcgal-dev libboost-all-dev libgl1-mesa-glx
